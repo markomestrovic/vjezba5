1d7818a66eeb7392dd69651ccb320c740d2689f4
kula124 <kula124@gmail.com>
2022-11-08 20:15:29 +0100

    Vjezba 4: Fix navigation linking

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,8 @@
 import { navigationItems } from '../constants/navbar';
 import { useRouter } from 'next/router';
 
+import Link from 'next/link';
+
 const NavBar = () => {
     const router = useRouter();
     const currentPage = router.pathname;
@@ -8,16 +10,18 @@ const NavBar = () => {
     return (
         <nav className="inline-flex list-none font-medium text-hci-lila">
             {navigationItems.map(({ label, path }) => (
-                <li
-                    key={label}
-                    className={`px-5 py-2 whitespace-nowrap hover:bg-hci-lila hover:bg-opacity-50 hover:text-white cursor-pointer ${
-                        currentPage === path
-                            ? 'text-hci-lila-light bg-hci-lila bg-opacity-60'
-                            : ''
-                    }`}
-                >
-                    {label}
-                </li>
+                <Link href={path} key={label} passHref>
+                    <li
+                        key={label}
+                        className={`px-5 py-2 whitespace-nowrap hover:bg-hci-lila hover:bg-opacity-50 hover:text-white cursor-pointer ${
+                            currentPage === path
+                                ? 'text-hci-lila-light bg-hci-lila bg-opacity-60'
+                                : ''
+                        }`}
+                    >
+                        {label}
+                    </li>
+                </Link>
             ))}
         </nav>
     );
